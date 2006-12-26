0a329bb8592249277252c8d746bfaac3d5dd28b2
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2006-12-26 20:05:32 +0000

    Fixed spanish messages due to lexical errors + local subversion errors that marked the other files as new. There are no changes from previous version.

--- Xinco/src/com/bluecubs/xinco/service/XincoSoapBindingSkeleton.java
+++ Xinco/src/com/bluecubs/xinco/service/XincoSoapBindingSkeleton.java
@@ -5,7 +5,7 @@
  * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
  */
 
-package com.bluecubs.xinco.service;
+package com.bluecubs.xinco.service; 
 
 public class XincoSoapBindingSkeleton implements com.bluecubs.xinco.service.Xinco, org.apache.axis.wsdl.Skeleton {
     private com.bluecubs.xinco.service.Xinco impl;
