7d6f1c91a812583929e7536aa4b77524742590c0
javydreamercsw <javydreamercsw@8f985321-f470-422e-a496-73dbe5d7d56d>
2007-01-11 22:07:26 +0000

    Eliminated debug code from AuditServer. Translated the new key.

--- trunk/Xinco/src/com/bluecubs/xinco/core/server/XincoCoreAuditServer.java
+++ trunk/Xinco/src/com/bluecubs/xinco/core/server/XincoCoreAuditServer.java
@@ -54,7 +54,6 @@ public class XincoCoreAuditServer {
                         sql+=")";
                 }
             }
-            System.out.println(sql);
             stmt.executeUpdate(sql);
             sql="insert into `xinco_core_user_modified_record` (`id`, `record_id`, `mod_Time`, " +
                     "`mod_Reason`) values ('"+id+"', "+record_ID+", '"+
