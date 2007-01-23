ea28fb4b0f04217e0f2473da83e28bb326732e08
bluecubs <bluecubs@8f985321-f470-422e-a496-73dbe5d7d56d>
2007-01-23 23:51:51 +0000

    - refresh on reconnect finally fixed - folder language pre-selection fixed - formatted log messages with blank between action and user input reason
    - made audit trails MySQL/PostgreSQL compatible
    - made display of audit trails in XincoAdmin MySQL/PostgreSQL compatible
    -> xinco DMS requires at least MySQL 4.1, not 4.0, to have all functionality available

--- trunk/XincoWebApp/src/com/bluecubs/xinco/server/XincoAdminServlet.java
+++ trunk/XincoWebApp/src/com/bluecubs/xinco/server/XincoAdminServlet.java
@@ -630,7 +630,7 @@ public class XincoAdminServlet extends HttpServlet {
         out.println("<title>XincoAdmin</title>");
         out.println("<link rel=\"stylesheet\" href=\"xincostyle.css\" type=\"text/css\"/>");
         out.println("</head>");
-        out.println("<body onload=\"document.forms[0].elements[0].focus();\">");
+        out.println("<body onload=\"if (document.forms[0] != null) { if (document.forms[0].elements[0] != null) { document.forms[0].elements[0].focus(); } }\">");
         out.println("<center>");
         out.println("<span class=\"text\">");
         
@@ -1220,9 +1220,9 @@ public class XincoAdminServlet extends HttpServlet {
                     if(request.getParameter("table").equals("xinco_core_data_type_attribute"))
                         column="xinco_core_data_type_id";
                     rs=DBM.con.createStatement().executeQuery("select * from "+request.getParameter("table")+
-                            "_t a, (select concat(concat(a.firstname,' '),a.name) '"+
-                            rb.getString("general.user")+"' , b.mod_time '"+rb.getString("general.audit.modtime")+
-                            "' ,b.mod_reason '"+rb.getString("general.reason")+"' ,b.record_id " +
+                            "_t a, (select a.firstname || ' ' || a.name as "+
+                            rb.getString("general.user").replaceAll(" ", "_")+" , b.mod_time as "+rb.getString("general.audit.modtime").replaceAll(" ", "_")+
+                            " ,b.mod_reason as "+rb.getString("general.reason").replaceAll(" ", "_")+" ,b.record_id " +
                             "from xinco_core_user a,xinco_core_user_modified_record b where a.id=b.id " +
                             ") b where b.record_id =a.record_id and a."+column+
                             " = '"+request.getParameter("id")+"' order by a.record_id desc");
