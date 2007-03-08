b2d4fcec38fc58d194e9185c102ab4881e5c2d6b
javydreamercsw <javydreamercsw@8f985321-f470-422e-a496-73dbe5d7d56d>
2007-03-08 21:30:44 +0000

    1. Added new keys. 2. Added support for pt_BR properties file. 3. The other files (java) had refactor changes only.

--- trunk/Xinco/src/com/bluecubs/xinco/messages/native2ascii.bat
+++ trunk/Xinco/src/com/bluecubs/xinco/messages/native2ascii.bat
@@ -6,4 +6,5 @@ native2ascii.exe _XincoMessages_fr.properties XincoMessages_fr.properties
 native2ascii.exe _XincoMessages_nl.properties XincoMessages_nl.properties
 native2ascii.exe _XincoMessages_pl.properties XincoMessages_pl.properties
 native2ascii.exe _XincoMessages_pt.properties XincoMessages_pt.properties
+native2ascii.exe _XincoMessages_pt_BR.properties XincoMessages_pt_BR.properties
 native2ascii.exe _XincoMessagesLocale.properties XincoMessagesLocale.properties
