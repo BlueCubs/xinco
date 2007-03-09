2fad6faec1ccacbff24a5ffc79a0ad1745f4fa9a
javydreamercsw <javydreamercsw@8f985321-f470-422e-a496-73dbe5d7d56d>
2007-03-09 13:52:51 +0000

    Copied remotely

---
+++ branches/2.0/Xinco/src/com/bluecubs/xinco/service/XincoService.java
@@ -0,0 +1,16 @@
+/**
+ * XincoService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.bluecubs.xinco.service;
+
+public interface XincoService extends javax.xml.rpc.Service {
+    public java.lang.String getXincoAddress();
+
+    public com.bluecubs.xinco.service.Xinco getXinco() throws javax.xml.rpc.ServiceException;
+
+    public com.bluecubs.xinco.service.Xinco getXinco(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
