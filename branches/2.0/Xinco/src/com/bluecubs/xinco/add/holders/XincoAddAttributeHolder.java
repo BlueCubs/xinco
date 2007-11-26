7253588eb5f8317dfe706759f076a17840fa42d0
javydreamercsw <javydreamercsw@8f985321-f470-422e-a496-73dbe5d7d56d>
2007-11-26 23:23:29 +0000

    From 2.0.3 RC2 (modified)

---
+++ branches/2.0/Xinco/src/com/bluecubs/xinco/add/holders/XincoAddAttributeHolder.java
@@ -0,0 +1,22 @@
+/**
+ * XincoAddAttributeHolder.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.bluecubs.xinco.add.holders;
+
+import com.bluecubs.xinco.core.XincoAddAttribute;
+
+public final class XincoAddAttributeHolder implements javax.xml.rpc.holders.Holder {
+    XincoAddAttribute value;
+
+    public XincoAddAttributeHolder() {
+    }
+
+    public XincoAddAttributeHolder(XincoAddAttribute value) {
+        this.value = value;
+    }
+
+}
