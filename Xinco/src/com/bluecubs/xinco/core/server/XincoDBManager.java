1f60785ddf841022a7497226178aaf69bd1afbb6
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2009-09-16 12:16:21 +0000

    Context files were missing a lot of configuration stuff that was causing trouble. It has been fixed.

--- Xinco/src/com/bluecubs/xinco/core/server/XincoDBManager.java
+++ Xinco/src/com/bluecubs/xinco/core/server/XincoDBManager.java
@@ -33,7 +33,6 @@
  *
  *************************************************************
  */
-
 package com.bluecubs.xinco.core.server;
 
 import javax.sql.DataSource;
@@ -51,44 +50,44 @@ import java.util.ResourceBundle;
 import java.util.StringTokenizer;
 
 public class XincoDBManager {
-    
+
     public Connection con;
     public XincoConfigSingletonServer config;
     public static int count = 0;
-    private int EmailLink=1,DataLink=2;
+    private int EmailLink = 1, DataLink = 2;
     private ResourceBundle lrb = null;
-    private Locale loc=null;
-    
+    private Locale loc = null;
+
     public XincoDBManager() throws Exception {
         lrb = ResourceBundle.getBundle("com.bluecubs.xinco.messages.XincoMessages");
         //load compiled configuartion
         config = XincoConfigSingletonServer.getInstance();
-        DataSource datasource = (DataSource)(new InitialContext()).lookup(config.JNDIDB);
+        DataSource datasource = (DataSource) (new InitialContext()).lookup(config.JNDIDB);
         con = datasource.getConnection();
         con.setAutoCommit(false);
         count++;
     }
-    
+
     public int getNewID(String attrTN) throws Exception {
-        
+
         int newID = 0;
         Statement stmt;
-        
+
         stmt = con.createStatement();
         ResultSet rs = stmt.executeQuery("SELECT * FROM xinco_id WHERE tablename='" + attrTN + "'");
         while (rs.next()) {
             newID = rs.getInt("last_id") + 1;
         }
         stmt.close();
-        
+
         stmt = con.createStatement();
         stmt.executeUpdate("UPDATE xinco_id SET last_id=last_id+1 WHERE tablename='" + attrTN + "'");
         stmt.close();
-        
+
         return newID;
-        
+
     }
-    
+
     @Override
     protected void finalize() throws Throwable {
         try {
@@ -101,117 +100,137 @@ public class XincoDBManager {
             super.finalize();
         }
     }
-    
-    /**Draws a table with results of the query stored in the ResultSet rs in the PrintWriter out*/
-    public void drawTable(ResultSet rs, PrintWriter out , String header, String title , int columnAsLink, boolean details, int linkType) {
-        try{
+//TODO: Replace with a report
+
+    /**
+     * Draws a table with results of the query stored in the ResultSet rs in the PrintWriter out
+     * @param rs
+     * @param out
+     * @param header
+     * @param title
+     * @param columnAsLink
+     * @param details
+     * @param linkType
+     */
+    public void drawTable(ResultSet rs, PrintWriter out, String header, String title, int columnAsLink, boolean details, int linkType) {
+        try {
             int size = rs.getMetaData().getColumnCount();
             out.println(title);
             out.println("<center><table border =1 ><tr>");
-            out.println(header+"</td></tr><tr>");
-            while(rs.next()){
-                for(int i=1;i<=size;i++) {
-                    String value=canReplace(rs.getString(i));
-                    if(rs.getMetaData().getColumnName(i).contains("password"))
+            out.println(header + "</td></tr><tr>");
+            while (rs.next()) {
+                for (int i = 1; i <= size; i++) {
+                    String value = canReplace(rs.getString(i));
+                    if (rs.getMetaData().getColumnName(i).contains("password")) {
                         value = "******************************";
-                    if(i==size && details) {
-                        out.println("<td><form action='Detail.jsp' method='post'><input type='submit' value='Get Details' onclick='Detail.jsp'><input type='hidden' name = 'key' value='"+
-                                value+"'><input type='hidden' name='Page' value='ProcessData.jsp'></form></td>");
+                    }
+                    if (i == size && details) {
+                        out.println("<td><form action='Detail.jsp' method='post'><input type='submit' value='Get Details' onclick='Detail.jsp'><input type='hidden' name = 'key' value='" +
+                                value + "'><input type='hidden' name='Page' value='ProcessData.jsp'></form></td>");
                     } else {
-                        if(i==columnAsLink && linkType==this.EmailLink){
-                            if(value==null)
+                        if (i == columnAsLink && linkType == this.EmailLink) {
+                            if (value == null) {
                                 out.println("<td>No email address available</td>");
-                            else
-                                out.println("<td><a href= mailto:"+value+">Email this person</a></td>");
+                            } else {
+                                out.println("<td><a href= mailto:" + value + ">Email this person</a></td>");
+                            }
                         }
-                        if(i==columnAsLink && linkType==this.DataLink){
-                            if(value==null)
+                        if (i == columnAsLink && linkType == this.DataLink) {
+                            if (value == null) {
                                 out.println("<td>No code available</td>");
-                            else
-                                out.println("<td>"+value+"</td><td><form action='Detail.jsp' method='post'><input type='submit' value='Get Details' onclick='Detail.jsp'><input type='hidden' name = 'key' value='"+
-                                        value+"'><input type='hidden' name='Page' value='Codes.jsp'></form></td>");
+                            } else {
+                                out.println("<td>" + value + "</td><td><form action='Detail.jsp' method='post'><input type='submit' value='Get Details' onclick='Detail.jsp'><input type='hidden' name = 'key' value='" +
+                                        value + "'><input type='hidden' name='Page' value='Codes.jsp'></form></td>");
+                            }
                         } else {
-                            if(value==null)
-                                out.println("<td>"+lrb.getString("general.nodata")+"</td>");
-                            else
-                                out.println("<td>"+value+"</td>");
+                            if (value == null) {
+                                out.println("<td>" + lrb.getString("general.nodata") + "</td>");
+                            } else {
+                                out.println("<td>" + value + "</td>");
+                            }
                         }
                     }
                 }
                 out.println("</tr><tr>");
             }
             out.println("</tr></table></center>");
-        } catch(Exception e) {
+        } catch (Exception e) {
             out.println(lrb.getString("general.nodata"));
             System.out.println("Exception drawing table: " + e.getMessage());
         }
     }
-    
+
     /** Returns the column names of the query in an HTML table format for use
      * as header for a table produced by the drawTable method.
      * @param rs
      * @return 
      */
     public StringTokenizer getColumnNamesList(ResultSet rs) {
-        String list="";
+        String list = "";
         StringTokenizer t;
-        try{
+        try {
             ResultSetMetaData rsmd = rs.getMetaData();
             int numColumns = rsmd.getColumnCount();
-            
+
             // Get the column names; column indices start from 1
-            for (int i=1; i<numColumns+1; i++) {
-                list+=rsmd.getColumnName(i)+",";
+            for (int i = 1; i < numColumns + 1; i++) {
+                list += rsmd.getColumnName(i) + ",";
             }
-        } catch(SQLException e) {
-            System.err.println("Error getting names from result set. "+e);
+        } catch (SQLException e) {
+            System.err.println("Error getting names from result set. " + e);
         }
-        t=new StringTokenizer(list,",");
+        t = new StringTokenizer(list, ",");
         return t;
     }
-    
-    /** Returns the column names of the query in an HTML table format for use
-     * as header for a table produced by the drawTable method.*/
+
+    /**
+     * Returns the column names of the query in an HTML table format for use
+     * as header for a table produced by the drawTable method.
+     * @param rs
+     * @return
+     */
     public String getColumnNames(ResultSet rs) {
-        String header="";
-        try{
+        String header = "";
+        try {
             ResultSetMetaData rsmd = rs.getMetaData();
             int numColumns = rsmd.getColumnCount();
-            
+
             // Get the column names; column indices start from 1
-            for (int i=1; i<numColumns+1; i++) {
-                header += "<td><b>"+rsmd.getColumnName(i)+"</b>";
+            for (int i = 1; i < numColumns + 1; i++) {
+                header += "<td><b>" + rsmd.getColumnName(i) + "</b>";
             }
-            header +="</td>";
-        } catch(SQLException e) {
-            System.err.println("Error getting names from result set. "+e);
+            header += "</td>";
+        } catch (SQLException e) {
+            System.err.println("Error getting names from result set. " + e);
         }
         return header;
     }
-    
+
     /*Replace a string with contents of resource bundle is applicable
      *Used to transform db contents to human readable form.
      */
-    private String canReplace(String s){
-        if(s==null)
+    private String canReplace(String s) {
+        if (s == null) {
             return null;
-        try{
+        }
+        try {
             lrb.getString(s);
-        }catch (MissingResourceException e){
+        } catch (MissingResourceException e) {
             return s;
         }
         return lrb.getString(s);
     }
-    
+
     public void setLoc(Locale loc) {
         this.loc = loc;
-        if (loc==null)
+        if (loc == null) {
             loc = Locale.getDefault();
-        else
+        } else {
             try {
-                lrb = ResourceBundle.getBundle("com.bluecubs.xinco.messages.XincoMessages",loc);
+                lrb = ResourceBundle.getBundle("com.bluecubs.xinco.messages.XincoMessages", loc);
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
     }
 }
