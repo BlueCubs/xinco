f8bf50af1e2cea9b1e211a7ecf3bff5913f53af0
javydreamercsw <javydreamercsw@8f985321-f470-422e-a496-73dbe5d7d56d>
2009-11-06 12:51:11 +0000

    To be replaced

--- branches/2.1 Hibernate/DB/Developer Environment.sql
+++
@@ -1,5 +0,0 @@
-/*Set Xinco in Developer mode for aditional debugging messages*/
-update xinco_setting set bool_value = 1 where id = 10;
-update xinco_setting set bool_value = 1 where id = 37;
-
-
--- branches/2.1 Hibernate/DB/xinco_upgrade_V2.00.00_to_V2.00.03_MySQL.sql
+++
@@ -1,5 +0,0 @@
-alter table xinco_core_user add unique (username);
-
-alter table xinco_core_group add unique (designation);
-
-alter table xinco_core_language add unique (designation);
