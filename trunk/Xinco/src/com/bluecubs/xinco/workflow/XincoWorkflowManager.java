a47f12d777bf6514e3bf1bfbab37baa36bbfe884
javydreamercsw <javydreamercsw@8f985321-f470-422e-a496-73dbe5d7d56d>
2009-12-31 05:28:18 +0000

    WIP of the workflow engine.

--- trunk/Xinco/src/com/bluecubs/xinco/workflow/XincoWorkflowManager.java
+++ trunk/Xinco/src/com/bluecubs/xinco/workflow/XincoWorkflowManager.java
@@ -1,40 +1,22 @@
 package com.bluecubs.xinco.workflow;
 
-import com.bigbross.bossa.Bossa;
-import com.bigbross.bossa.BossaFactory;
-import com.bigbross.bossa.PersistenceException;
-import com.bigbross.bossa.wfnet.CaseTypeManager;
-import java.io.File;
+import javax.persistence.PersistenceException;
 
 /**
  *
  * @author Javier A. Ortiz Bultrón <javier.ortiz.78@gmail.com>
  */
-public class XincoWorkflowManager extends CaseTypeManager {
+public class XincoWorkflowManager {
 
     private static XincoWorkflowManager instance = null;
-    private static Bossa engine = null;
 
-    private XincoWorkflowManager(Bossa engine) {
-        super(engine);
+    private XincoWorkflowManager() {
     }
 
     public static XincoWorkflowManager get() throws PersistenceException {
         if (instance == null) {
-            instance = new XincoWorkflowManager(getBossaEngine());
+            instance = new XincoWorkflowManager();
         }
         return instance;
-    }
-
-    private static Bossa getBossaEngine() throws PersistenceException {
-        if (engine == null) {
-            BossaFactory factory = new BossaFactory();
-            //TODO: should be configurable
-            String path="C:\\Temp\\Xinco\\Workflow";
-            new File(path).mkdirs();
-            factory.setStateDir(path);
-            engine = factory.createBossa();
-        }
-        return engine;
     }
 }
