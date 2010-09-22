628e15b3bb100bd0c0879b6d3bbd0b96ea6f20ff
javydreamercsw <javydreamercsw@8f985321-f470-422e-a496-73dbe5d7d56d>
2010-09-22 01:59:03 +0000

    Final release of version 2.00.09!

---
+++ tags/2.00.09/Xinco/src/com/bluecubs/xinco/tools/MD5.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bluecubs.xinco.tools;
+
+import com.bluecubs.xinco.core.XincoException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Javier A. Ortiz
+ */
+public class MD5 {
+
+    private static String md5val = "";
+    private static MessageDigest algorithm = null;
+
+    public static void main(String[] args) {
+        if (args.length != 0) {
+            for (int i = 0; i < args.length; i++) {
+                try {
+                    System.out.println("MD5 (" + args[i] + ") = " + encrypt(args[i]));
+                } catch (XincoException ex) {
+                    Logger.getLogger(MD5.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        } else {
+            try {
+                System.out.println("MD5 (admin) = " + encrypt("admin"));
+            } catch (XincoException ex) {
+                Logger.getLogger(MD5.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public static String encrypt(String text) throws XincoException {
+        try {
+            algorithm = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException nsae) {
+            throw new XincoException("Cannot find digest algorithm");
+        }
+        byte[] defaultBytes = text.getBytes();
+        algorithm.reset();
+        algorithm.update(defaultBytes);
+        byte messageDigest[] = algorithm.digest();
+        StringBuilder hexString = new StringBuilder();
+
+        for (int i = 0; i < messageDigest.length; i++) {
+            String hex = Integer.toHexString(0xFF & messageDigest[i]);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        md5val = hexString.toString();
+        return md5val;
+    }
+}
