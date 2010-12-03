c18c4823a0359f9a0a4c367b5baa9b317b077e47
javydreamercsw <javydreamercsw@8f985321-f470-422e-a496-73dbe5d7d56d>
2010-12-03 17:35:19 +0000

    2.00.10 RC3 Re-Tag

---
+++ tags/2.00.10 RC3/Xinco/src/com/bluecubs/xinco/core/XincoDataStatus.java
@@ -0,0 +1,13 @@
+package com.bluecubs.xinco.core;
+
+/**
+ *
+ * @author Javier A. Ortiz Bultrón <javier.ortiz.78@gmail.com>
+ */
+public enum XincoDataStatus {
+OPEN,
+LOCKED,
+ARCHIVED,
+CHECKED_OUT,
+PUBLISHED;
+}
