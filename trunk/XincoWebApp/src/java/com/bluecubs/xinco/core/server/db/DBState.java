14f2f540311b26e5aaf16c1aeaada67dbab76227
javydreamercsw <javydreamercsw@8f985321-f470-422e-a496-73dbe5d7d56d>
2011-08-16 21:03:02 +0000

    Database states

---
+++ trunk/XincoWebApp/src/java/com/bluecubs/xinco/core/server/db/DBState.java
@@ -0,0 +1,53 @@
+package com.bluecubs.xinco.core.server.db;
+
+import java.util.ResourceBundle;
+
+/**
+ * Enumeration to describe the database state
+ * @author Javier A. Ortiz Bultrón <javier.ortiz.78@gmail.com>
+ */
+public enum DBState {
+
+    /*
+     * Database is up to date
+     */
+    VALID("message.db.valid"),
+    /*
+     * Database needs a manual update
+     * (previous to 2.1.x the database version is not stored within the database)
+     */
+    NEED_MANUAL_UPDATE("error.old.db"),
+    /*
+     * Database just updated
+     */
+    UPDATED("message.updated.db"),
+    /*
+     * Database needs to be updated
+     */
+    NEED_UPDATE("message.update.db"),
+    /*
+     * Database needs initialization
+     */
+    NEED_INIT("message.init.db"),
+    /*
+     * Error detected
+     */
+    ERROR("message.db.error");
+    private final String mess;
+    private static ResourceBundle lrb = ResourceBundle.getBundle("com.bluecubs.xinco.messages.XincoMessages");
+
+    DBState(String mess) {
+        this.mess = mess;
+    }
+
+    /**
+     * @return the mess
+     */
+    public String getMessage() {
+        if (lrb.containsKey(mess)) {
+            return lrb.getString(mess);
+        } else {
+            return mess;
+        }
+    }
+}
