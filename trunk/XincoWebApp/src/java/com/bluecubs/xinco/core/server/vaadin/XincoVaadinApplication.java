368febdc7c7f10476df1282f441744a91cde22d1
javydreamercsw <javydreamercsw@8f985321-f470-422e-a496-73dbe5d7d56d>
2011-12-14 22:09:08 +0000

    Completed implementation of index search.

--- trunk/XincoWebApp/src/java/com/bluecubs/xinco/core/server/vaadin/XincoVaadinApplication.java
+++ trunk/XincoWebApp/src/java/com/bluecubs/xinco/core/server/vaadin/XincoVaadinApplication.java
@@ -6,6 +6,7 @@ import com.vaadin.ui.Tree;
 import com.vaadin.ui.Window;
 import com.vaadin.ui.Window.ResizeListener;
 import java.net.MalformedURLException;
+import java.util.List;
 import java.util.ResourceBundle;
 
 /**
@@ -28,23 +29,41 @@ public interface XincoVaadinApplication extends ResizeListener {
      * Reset activity timer
      */
     void resetTimer();
-    
+
     /**
      * Get the tree of the application
+     *
      * @return Tree
      */
     Tree getXincoTree();
 
     /**
      * Get main window
+     *
      * @return Main Window
      */
     public Window getMainWindow();
-    
+
     /**
      * Get service
+     *
      * @return Xinco Service
-     * @throws MalformedURLException 
+     * @throws MalformedURLException
      */
     public Xinco_Service getService() throws MalformedURLException;
+
+    /**
+     * Expand a node in the tree
+     *
+     * @param parents Node IDs to expand
+     * @return true if successful
+     */
+    public boolean expandTreeNodes(List<Integer> parents);
+    
+    /**
+     * Select node in tree
+     * @param nodeId Id to select (use "node-" for nodes and "data-" for data)
+     * @return true if successful
+     */
+    public boolean selectNode(String nodeId);
 }
