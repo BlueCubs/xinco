c56b1eda964aff132ea9feabd1b33ab7c2849cb3
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2012-01-03 23:27:06 +0000

    Send emails to admins when a XincoException is thrown

--- XincoWebApp/src/java/com/bluecubs/xinco/core/server/XincoCoreGroupServer.java
+++ XincoWebApp/src/java/com/bluecubs/xinco/core/server/XincoCoreGroupServer.java
@@ -1,50 +1,45 @@
 /**
- *Copyright 2011 blueCubs.com
+ * Copyright 2011 blueCubs.com
  *
- *Licensed under the Apache License, Version 2.0 (the "License");
- *you may not use this file except in compliance with the License.
- *You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *Unless required by applicable law or agreed to in writing, software
- *distributed under the License is distributed on an "AS IS" BASIS,
- *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *See the License for the specific language governing permissions and
- *limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  *
  *************************************************************
- * This project supports the blueCubs vision of giving back
- * to the community in exchange for free software!
- * More information on: http://www.bluecubs.org
- *************************************************************
+ * This project supports the blueCubs vision of giving back to the community in
+ * exchange for free software! More information on: http://www.bluecubs.org
+ * ************************************************************
  *
- * Name:            XincoCoreGroupServer
+ * Name: XincoCoreGroupServer
  *
- * Description:     group
+ * Description: group
  *
- * Original Author: Alexander Manes
- * Date:            2004
+ * Original Author: Alexander Manes Date: 2004
  *
  * Modifications:
  *
- * Who?             When?             What?
- * -                -                 -
+ * Who? When? What? - - -
  *
  *************************************************************
  */
 package com.bluecubs.xinco.core.server;
 
 import com.bluecubs.xinco.core.XincoException;
+import com.bluecubs.xinco.core.server.persistence.XincoCoreUserHasXincoCoreGroup;
 import com.bluecubs.xinco.core.server.persistence.controller.XincoCoreGroupJpaController;
 import com.bluecubs.xinco.core.server.persistence.controller.exceptions.IllegalOrphanException;
 import com.bluecubs.xinco.core.server.persistence.controller.exceptions.NonexistentEntityException;
 import com.bluecubs.xinco.core.server.service.XincoCoreGroup;
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -91,7 +86,7 @@ public class XincoCoreGroupServer extends XincoCoreGroup {
     public int write2DB() throws XincoException {
         try {
             XincoCoreGroupJpaController controller = new XincoCoreGroupJpaController(XincoDBManager.getEntityManagerFactory());
-            com.bluecubs.xinco.core.server.persistence.XincoCoreGroup xcg =null;
+            com.bluecubs.xinco.core.server.persistence.XincoCoreGroup xcg;
             if (getId() > 0) {
                 xcg = controller.findXincoCoreGroup(getId());
                 xcg.setDesignation(getDesignation().replaceAll("'", "\\\\'"));
@@ -116,7 +111,7 @@ public class XincoCoreGroupServer extends XincoCoreGroup {
         return getId();
     }
 
-    public static int deleteFromDB(XincoCoreGroup group){
+    public static int deleteFromDB(XincoCoreGroup group) {
         try {
             new XincoCoreGroupJpaController(XincoDBManager.getEntityManagerFactory()).destroy(group.getId());
             return 0;
@@ -135,12 +130,25 @@ public class XincoCoreGroupServer extends XincoCoreGroup {
         ArrayList coreGroups = new ArrayList();
         try {
             result = XincoDBManager.createdQuery("SELECT xcg FROM XincoCoreGroup xcg ORDER BY xcg.designation");
-            for(Object o: result) {
+            for (Object o : result) {
                 coreGroups.add(new XincoCoreGroupServer((com.bluecubs.xinco.core.server.persistence.XincoCoreGroup) o));
             }
         } catch (Exception e) {
             coreGroups.clear();
         }
         return coreGroups;
+    }
+
+    protected static List<XincoCoreUserServer> getMembersOfGroup(int id) {
+        parameters.clear();
+        parameters.put("xincoCoreGroupId", id);
+        result = XincoDBManager.namedQuery("XincoCoreUserHasXincoCoreGroup.findByXincoCoreGroupId",
+                parameters);
+        ArrayList<XincoCoreUserServer> users = new ArrayList<XincoCoreUserServer>();
+        for (Iterator it = result.iterator(); it.hasNext();) {
+            XincoCoreUserHasXincoCoreGroup xcuhg = (XincoCoreUserHasXincoCoreGroup) it.next();
+            users.add(new XincoCoreUserServer(xcuhg.getXincoCoreUser()));
+        }
+        return users;
     }
 }
