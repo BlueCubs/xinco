cf1abdb9aa614d5621cbea2db894c3c0300e5cf8
Javier A. Ortiz Bultron <javier.ortiz.78@gmail.com>
2012-01-03 23:09:43 +0000

    Add tool to validate email addresses

--- XincoWebApp/src/java/com/bluecubs/xinco/tools/Tool.java
+++ XincoWebApp/src/java/com/bluecubs/xinco/tools/Tool.java
@@ -8,6 +8,8 @@ import java.net.DatagramSocket;
 import java.net.ServerSocket;
 import java.nio.channels.FileChannel;
 import java.util.StringTokenizer;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 
 /**
  *
@@ -119,5 +121,58 @@ public class Tool {
                 destination.close();
             }
         }
+    }
+
+    /**
+     * Validate the form of an email address.
+     *
+     * <P>Return <tt>true</tt> only if <ul> <li> <tt>aEmailAddress</tt> can
+     * successfully construct an
+     * {@link javax.mail.internet.InternetAddress} <li> when parsed with "@" as
+     * delimiter, <tt>aEmailAddress</tt> contains two tokens which satisfy {@link hirondelle.web4j.util.Util#textHasContent}.
+     * </ul>
+     *
+     * <P> The second condition arises since local email addresses, simply of
+     * the form "<tt>albert</tt>", for example, are valid for
+     * {@link javax.mail.internet.InternetAddress}, but almost always undesired.
+     */
+    public static boolean isValidEmailAddress(String aEmailAddress) {
+        if (aEmailAddress == null) {
+            return false;
+        }
+        boolean result = true;
+        try {
+            InternetAddress emailAddr = new InternetAddress(aEmailAddress);
+            if (!hasNameAndDomain(aEmailAddress)) {
+                result = false;
+            }
+        } catch (AddressException ex) {
+            result = false;
+        }
+        return result;
+    }
+
+    private static boolean hasNameAndDomain(String aEmailAddress) {
+        String[] tokens = aEmailAddress.split("@");
+        return tokens.length == 2
+                && textHasContent(tokens[0])
+                && textHasContent(tokens[1]);
+    }
+
+    /**
+     * Return <tt>true</tt> only if <tt>aText</tt> is not null, and is not empty
+     * after trimming. (Trimming removes both leading/trailing whitespace and
+     * ASCII control characters.)
+     *
+     * <P> For checking argument validity, {@link Args#checkForContent} should
+     * be used instead of this method.
+     *
+     * <P>This method is particularly useful, since it is very commonly
+     * required.
+     *
+     * @param aText possibly-null.
+     */
+    public static boolean textHasContent(String aText) {
+        return (aText != null) && (aText.trim().length() > 0);
     }
 }
