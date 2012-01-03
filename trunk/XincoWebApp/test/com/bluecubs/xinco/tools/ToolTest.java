484530add199929df2fb605ceb701a9990faefb2
javydreamercsw <javydreamercsw@8f985321-f470-422e-a496-73dbe5d7d56d>
2012-01-03 22:44:26 +0000

    Completing unit test.

--- trunk/XincoWebApp/test/com/bluecubs/xinco/tools/ToolTest.java
+++ trunk/XincoWebApp/test/com/bluecubs/xinco/tools/ToolTest.java
@@ -1,7 +1,11 @@
 package com.bluecubs.xinco.tools;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import java.io.File;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static org.junit.Assert.*;
 import org.junit.*;
 
 /**
@@ -9,7 +13,7 @@ import org.junit.*;
  * @author Javier A. Ortiz Bultrón <javier.ortiz.78@gmail.com>
  */
 public class ToolTest {
-    
+
     public ToolTest() {
     }
 
@@ -20,11 +24,11 @@ public class ToolTest {
     @AfterClass
     public static void tearDownClass() throws Exception {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -53,5 +57,61 @@ public class ToolTest {
         String first = "2,1,0";
         String second = "2,01,00";
         assertTrue(Tool.compareNumberStrings(first, second, ","));
+    }
+
+    /**
+     * Test of compareNumberStrings method, of class Tool.
+     */
+    @Test
+    public void testCompareNumberStrings_3args() {
+        System.out.println("compareNumberStrings");
+        String first = "2,1,0";
+        String second = "2,01,00";
+        String separator = ",";
+        assertTrue(Tool.compareNumberStrings(first, second, separator));
+        separator = ".";
+        assertFalse(Tool.compareNumberStrings(first, second, separator));
+        second = "3.1.0";
+        assertFalse(Tool.compareNumberStrings(first, second, separator));
+        second = "2.1";
+        assertFalse(Tool.compareNumberStrings(first, second, separator));
+        second = "M";
+        assertFalse(Tool.compareNumberStrings(first, second, separator));
+    }
+
+    /**
+     * Test of isPortAvaialble method, of class Tool.
+     */
+    @Test
+    public void testIsPortAvaialble() {
+        try {
+            System.out.println("isPortAvaialble");
+            int port = 9019;
+            assertTrue(Tool.isPortAvaialble(port));
+            ServerSocket ss = new ServerSocket(port);
+            assertFalse(Tool.isPortAvaialble(port));
+            ss.close();
+            assertTrue(Tool.isPortAvaialble(port));
+        } catch (IOException ex) {
+            Logger.getLogger(ToolTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail();
+        }
+    }
+
+    /**
+     * Test of copyFile method, of class Tool.
+     */
+    @Test
+    public void testCopyFile() throws Exception {
+        System.out.println("copyFile");
+        File sourceFile = new File("demo.txt");
+        sourceFile.createNewFile();
+        sourceFile.deleteOnExit();
+        assertTrue(sourceFile.exists());
+        File destFile = new File("demo2.txt");
+        assertFalse(destFile.exists());
+        Tool.copyFile(sourceFile, destFile);
+        destFile.deleteOnExit();
+        assertTrue(destFile.exists());
     }
 }
