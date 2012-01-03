82393e459b27b4ea5820bbb3e77c6403816b97f8
javydreamercsw <javydreamercsw@8f985321-f470-422e-a496-73dbe5d7d56d>
2012-01-03 18:59:22 +0000

    Provide methods for copying files and checking TCP port availability.

--- trunk/XincoWebApp/src/java/com/bluecubs/xinco/tools/Tool.java
+++ trunk/XincoWebApp/src/java/com/bluecubs/xinco/tools/Tool.java
@@ -1,5 +1,12 @@
 package com.bluecubs.xinco.tools;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+import java.nio.channels.FileChannel;
 import java.util.StringTokenizer;
 
 /**
@@ -8,31 +15,30 @@ import java.util.StringTokenizer;
  */
 public class Tool {
 
+    public static int MIN_PORT_NUMBER = 1;
+    public static int MAX_PORT_NUMBER = 65535;
+
     private Tool() {
     }
 
     /**
-     * Compare two number strings.
-     * For example:
-     * 2.1.0 == 2.01.00
-     * 
+     * Compare two number strings. For example: 2.1.0 == 2.01.00
+     *
      * @param first first string to compare
      * @param second second string to compare
-     * @return 
+     * @return
      */
     public static boolean compareNumberStrings(String first, String second) {
         return compareNumberStrings(first, second, ".");
     }
 
     /**
-     * Compare two number strings.
-     * For example:
-     * 2.1.0 == 2.01.00
-     * 
+     * Compare two number strings. For example: 2.1.0 == 2.01.00
+     *
      * @param first first string to compare
      * @param second second string to compare
      * @param separator separator of fields (i.e. for 2.1.0 is '.')
-     * @return 
+     * @return
      */
     public static boolean compareNumberStrings(String first, String second,
             String separator) {
@@ -58,5 +64,60 @@ public class Tool {
             }
         }
         return true;
+    }
+
+    public static boolean isPortAvaialble(int port) {
+        if (port < MIN_PORT_NUMBER || port > MAX_PORT_NUMBER) {
+            throw new IllegalArgumentException("Invalid start port: " + port);
+        }
+
+        ServerSocket ss = null;
+        DatagramSocket ds = null;
+        try {
+            ss = new ServerSocket(port);
+            ss.setReuseAddress(true);
+            ds = new DatagramSocket(port);
+            ds.setReuseAddress(true);
+            return true;
+        } catch (IOException e) {
+        } finally {
+            if (ds != null) {
+                ds.close();
+            }
+
+            if (ss != null) {
+                try {
+                    ss.close();
+                } catch (IOException e) {
+                    /*
+                     * should not be thrown
+                     */
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void copyFile(File sourceFile, File destFile) throws IOException {
+        if (!destFile.exists()) {
+            destFile.createNewFile();
+        }
+
+        FileChannel source = null;
+        FileChannel destination = null;
+
+        try {
+            source = new FileInputStream(sourceFile).getChannel();
+            destination = new FileOutputStream(destFile).getChannel();
+            destination.transferFrom(source, 0, source.size());
+        } finally {
+            if (source != null) {
+                source.close();
+            }
+            if (destination != null) {
+                destination.close();
+            }
+        }
     }
 }
