da2aa6fade02cb109d745fb172affccbad65c3d0
javydreamercsw <javydreamercsw@8f985321-f470-422e-a496-73dbe5d7d56d>
2017-04-25 20:07:17 +0000

    Add 2.00.05 to 2.00.11 changes.

---
+++ trunk/DB/xinco_upgrade_V2.00.05_to_V2.00.011_PostgreSQL.sql
@@ -0,0 +1,2 @@
+INSERT INTO xinco_setting (id, description, int_value, string_value, bool_value, long_value) VALUES(39,'password.max',-1,null,false,null);
+INSERT INTO xinco_setting (id, description, int_value, string_value, bool_value, long_value) VALUES(40,'password.min',-1,null,false,null);
