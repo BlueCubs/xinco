7f0978c5bffd68a56c5172401dc2a051359945ff
javydreamercsw <javydreamercsw@8f985321-f470-422e-a496-73dbe5d7d56d>
2017-04-25 20:54:57 +0000

    Update status to aged.

--- trunk/DB/xinco_upgrade_V2.00.05_to_V2.00.11_MySQL.sql
+++ trunk/DB/xinco_upgrade_V2.00.05_to_V2.00.11_MySQL.sql
@@ -1,4 +1,4 @@
 INSERT INTO xinco_setting (id, description, int_value, string_value, bool_value, long_value) VALUES(39,'password.max',-1,null,false,null);
 INSERT INTO xinco_setting (id, description, int_value, string_value, bool_value, long_value) VALUES(40,'password.min',-1,null,false,null);
 /*Mark all users to change password*/
-update xinco_core_user set status_number = 4;
+update xinco_core_user set status_number = 3;
