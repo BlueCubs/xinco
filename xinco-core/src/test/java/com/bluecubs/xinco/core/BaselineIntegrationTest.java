b6f18691ed738df72b532d4a316b34cc080a1f24
Javier Ortiz Bultron <javier.ortiz.78@gmail.com>
2026-01-18 22:04:49 -0600

    test(baseline): Add failsafe plugin and baseline tests

---
+++ xinco-core/src/test/java/com/bluecubs/xinco/core/BaselineIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.bluecubs.xinco.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BaselineIntegrationTest {
+
+  @Test
+  public void testIntegrationSanity() {
+    assertTrue("Baseline integration sanity check", true);
+  }
+}
---
+++ xinco-core/src/test/java/com/bluecubs/xinco/core/BaselineSanityTest.java
@@ -0,0 +1,13 @@
+package com.bluecubs.xinco.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BaselineSanityTest {
+
+  @Test
+  public void testSanity() {
+    assertTrue("Baseline sanity check", true);
+  }
+}
